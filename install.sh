e0c8dbc51abf82e2da585fdbd06d53f2fffaf28d
Stelios <stelios05+git@gmail.com>
2018-11-04 18:25:28 +0000

    exit on error, setup .aws,.ssh and other secrets from the private directory

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
-#!/bin/bash -x
+#!/bin/bash -x 
+set -o errexit
 DIR="$(dirname $(realpath '$0'))"
+
 #https://github.com/Bash-it/bash-it.git
-mkdir ~/bin/
+[[ -d ~/bin ]] || mkdir -pf ~/bin/
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/bin/git-completion.bash
 
 ln -fs ${DIR?}/.bash_profile ~/
@@ -9,3 +11,9 @@ ln -fs ${DIR?}/.bash_profile ~/
 npm config set prefix ~/npm
 
 pip install git-remote-dropbox
+
+ecryptfs-mount-private
+ln -sf ~/Private/dotfiles/.aws ~/.aws
+ln -sf ~/Private/dotfiles/.ssh ~/.ssh
+[[ -d ~/.config/git/ ]] || mkdir -p ~/.config/git
+ln -sf ~/Private/dotfiles/git-remote-dropbox.json ~/.config/git/git-remote-dropbox.json
